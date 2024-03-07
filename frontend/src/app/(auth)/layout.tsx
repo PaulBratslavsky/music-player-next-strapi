5a414cffa59750035871ce6901770a13aa554ac9
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2024-03-06 19:33:52 -0600

    update image

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout({ children }: {
 }) {
   return (
     <div className="relative flex flex-col items-center justify-center h-[calc(100vh-64px)] bg-gray-100 dark:bg-gray-900">
-      <BackgroundImage backgroundImage="/uploads/paulbratslavsky_cyberpunk_girl_in_the_style_of_bladerunner_movi_3e54ca01_3112_45ee_8660_0c8d09daf009_53caf9d901.png" />
+      <BackgroundImage backgroundImage="https://thoughtful-chickens-9661b5da26.media.strapiapp.com/paulbratslavsky_cyberpunk_girl_in_the_style_of_bladerunner_movi_55050682_5a79_4025_be2f_d92e07aa9c99_74468dcf36.png" />
       <div className="z-10 w-full max-w-md">{children}</div>
     </div>
   );
